ed358588d5e09e602a3f3a59308653dff30cd1f6
황선혜 <ghkdtjs6451@gmail.com>
2020-07-12 15:54:26 +0900

    메인 뷰페이저 연결 작업 완료

--- app/src/main/java/kr/co/tjoeun/viewpager_20200712/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200712/MainActivity.kt
@@ -2,8 +2,12 @@ package kr.co.tjoeun.viewpager_20200712
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.viewpager_20200712.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,9 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupValues() {
+
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
 
     }
 }
