cc43c58ba847fe45ee3726451ab7f5b573c55533
황선혜 <ghkdtjs6451@gmail.com>
2020-07-12 16:18:13 +0900

    어댑터의 리턴을 코틀린 return when 활용 예시

--- app/src/main/java/kr/co/tjoeun/viewpager_20200712/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200712/adapters/MainViewPagerAdapter.kt
@@ -9,18 +9,16 @@ import kr.co.tjoeun.fragment_20200712.fragments.SecondFragment
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
-        if(position == 0){
-            return "첫페이지"
-        }else{
-            return "두번째페이지"
+        return when(position){
+            0 -> "첫페이지"
+            else -> "두번째 페이지"
         }
     }
 
     override fun getItem(position: Int): Fragment {
-        if(position == 0){
-            return FirstFragment()
-        }else{
-            return SecondFragment()
+        return when(position){
+            0 -> FirstFragment()
+            else -> SecondFragment()
         }
     }
 
